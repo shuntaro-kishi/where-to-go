251cba28982652e820d6257af91cc977385e3a55
shuntaro-kishi <shuntaro.kishi@seattleconsulting.co.jp>
2020-11-27 01:27:09 +0900

    feat:ルーレット的な動き

---
+++ src/components/forms/CustomButton.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.custom-button
+  button(
+    @click="onClick"
+  )
+    | {{ text }}
+</template>
+
+<script>
+export default {
+  name: 'CustomButton',
+
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.$emit('on-click')
+    }
+  }
+}
+</script>
---
+++ src/components/layouts/ShowStation.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.show-station
+  .start-message(
+    v-if="randomIndex === -1"
+  )
+    | 行き先を決めましょう
+  .station-text(
+    v-else
+  )
+    | {{ stations[randomIndex] }}
+</template>
+
+<script>
+export default {
+  name: 'ShowStation',
+
+  props: {
+    stations: {
+      type: Array,
+      required: true
+    },
+    randomIndex: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.show-station {
+  font-size: 200px;
+}
+</style>
